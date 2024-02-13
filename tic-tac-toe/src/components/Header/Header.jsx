ae9836a2044e54d7d2837058ba2698f77afe7524
Edidiong Ukem <edidionge.ukem@gmail.com>
2024-02-13 13:35:20 +0100

    Creating Components

---
+++ tic-tac-toe/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="game-logo.png" alt="Hand-drawn Tic-Tac-Toe game board" />
+      <h1>React Tic-Tac-Toe</h1>
+    </header>
+  );
+};
+
+export default Header;
